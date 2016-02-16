0ea5ae5af3ead2f01c1ad1918095774f18478350
Deepal Suranga Weerasuriya <deepal.suranga29@gmail.com>
2016-02-17 02:58:52 +0530

    menu edited

--- xyzzyx_studentPortal/src/User_SendMailSSL.java
+++ xyzzyx_studentPortal/src/User_SendMailSSL.java
@@ -8,11 +8,7 @@
 /*
  * @author deepalsuranga
  */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
